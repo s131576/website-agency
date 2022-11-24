6c6f2b130a24203deda3f4ab536d7dfa739dad2f
Rachad Bouhjar <s131576@ap.be>
2022-11-24 17:19:18 +0100

    Home page: opmaak en styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,16 @@
-import * as React from 'react'
-import { Link, useStaticQuery, graphql } from 'gatsby'
-import { 
-  container, 
-  nav, 
-  navLinks, 
-  navLinkItem, 
-  navLinkText, 
-  siteTitle 
-} from './layout.module.css'
+import * as React from "react"
+import { Link, useStaticQuery, graphql } from "gatsby"
+import {
+  container,
+  nav,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+  siteTitle,
+} from "./layout.module.css"
+import Footer from "./footer"
 
-const Layout = ({ pageTitle, children }) => {
+const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query {
       site {
@@ -17,41 +18,50 @@ const Layout = ({ pageTitle, children }) => {
           title
         }
       }
+      wpPage(slug: { eq: "contact-us" }) {
+        contactUsFields {
+            address
+            city
+            zipCode
+            facebook
+            instagram
+        }
+      }
     }
   `)
 
-  return (
-    <div className={container}>
-      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-      <nav className={nav}>
-        <header className={siteTitle}>
-          <h1>{data.site.siteMetadata.title}</h1>
-        </header>
-        <ul className={navLinks}>
-        <li>
-        </li>
-          <li className={navLinkItem}>
-            <Link className={navLinkText} to="/">
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link className={navLinkText} to="/about">
-              About
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link className={navLinkText} to="/artists">
-              Artists
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <h1>{pageTitle}</h1>
-        {children}
-      </main>
-    </div>
+   return (
+    <>
+      <div className={container}>
+        <title>{data.site.siteMetadata.title}</title>
+        <nav className={nav}>
+          <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+          <ul className={navLinks}>
+            <li></li>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/">
+                Home
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/about">
+                About
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/artists">
+                Artists
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <main>{children}</main>
+      </div>
+      <Footer
+        siteTitle={data.site.siteMetadata.title}
+        companyInfo={data.wpPage.contactUsFields}
+      />
+    </>
   )
 }
 
