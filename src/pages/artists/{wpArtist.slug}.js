b0274ec0c7ca0cc3c4d44c2789ed493fe7e63373
Rachad Bouhjar <s131576@ap.be>
2022-11-24 09:11:56 +0100

    Toevoegen van dynamische afbeeldingen finished

--- src/pages/artists/{wpArtist.slug}.js
+++ src/pages/artists/{wpArtist.slug}.js
@@ -1,11 +1,14 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../../components/layout'
+import { GatsbyImag,GatsbyImage,getImage } from 'gatsby-plugin-image'
 
 const ArtistPage = ({data: {wpArtist: {artistMeta: artist}}}) => {
+  const image = getImage(artist.profilePicture.localFile)
   return (
     <Layout pageTitle="Artiesten Template">
     <div>
+     <GatsbyImage image={image} alt={artist.profilePicture.altText}/>
       <h3>{artist.artistName}</h3>
       <h1>{artist.firstName} {artist.lastName}</h1>
       <div dangerouslySetInnerHTML={{__html: artist.description}} />
@@ -21,22 +24,30 @@ const ArtistPage = ({data: {wpArtist: {artistMeta: artist}}}) => {
 }
 
 export const query = graphql`
-  query ($id: String) {
-    wpArtist(id: {eq: $id}) {
-      artistMeta {
-        firstName
-        lastName
-        email
-        description
-        artistName
-        height
-        origin
-        phoneNumber
-        shirtSize
-        shoeSize
+query ($slug: String) {
+  wpArtist(slug: {eq: $slug}) {
+    artistMeta {
+      firstName
+      lastName
+      email
+      description
+      artistName
+      height
+      origin
+      phoneNumber
+      shirtSize
+      shoeSize
+      profilePicture {
+        localFile {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED)
+          }
+        }
+        altText
       }
     }
   }
+}
 `
 
 export default ArtistPage
